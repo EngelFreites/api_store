2c9676252479f657b6ba0052802a0324f8f67746
engelbert <engelfreites97@gmail.com>
2023-07-29 16:36:42 -0500

    feat: create middelware handlerError

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express")
 const appRoutes = require("./routes/index")
 const app = express()
+const {handleError, logErrores, boomError} = require('./middleware/error.handler')
 
+app.use(express.json())
 appRoutes(app)
+
+app.use(logErrores)
+app.use(boomError)
+app.use(handleError)
+
 
 const PORT = 3001
 
