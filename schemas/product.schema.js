3db15e14791c671b2a933eab9de2a37c5d480bc6
engelbert <engelfreites97@gmail.com>
2023-07-29 16:37:47 -0500

    feat: create schema validate with joi libary

---
+++ schemas/product.schema.js
@@ -0,0 +1,24 @@
+const Joi = require("joi");
+
+const id = Joi.number().integer()
+const name = Joi.string().min(3).max(15)
+const price = Joi.number().integer().min(10)
+const img = Joi.string().required()
+
+const createProductSchema = Joi.object ({
+    name: name.required(),
+    price: price.required(),
+    img: img
+})
+
+const upDateProductSchema = Joi.object ({
+    name: name,
+    price: price,
+
+})
+
+const getProductSchema = Joi.object ({
+    id: id.required()
+})
+
+module.exports = { createProductSchema, upDateProductSchema, getProductSchema}
