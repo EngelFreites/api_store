e1cc7baac85502c48a20b416b42b797cac92c688
engelbert <engelfreites97@gmail.com>
2023-07-28 22:20:01 -0500

    feat:  create principals urls

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express")
 const app = express()
 
-app.get("/", (req, res) => {
+app.get("/products", (req, res) => {
+    res.send("hellow world")
+})
+
+app.get("/products/:id", (req, res) => {
+    res.send("hellow world")
+})
+
+app.get("/users", (req, res) => {
+    res.send("hellow world")
+})
+
+app.get("/users/:id", (req, res) => {
     res.send("hellow world")
 })
 
